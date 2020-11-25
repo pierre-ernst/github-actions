a49f7ee62db912fdefefc94e3862aff0f3ac5147
Pierre Ernst <pierre-ernst@users.noreply.github.com>
2020-11-25 09:49:52 -0500

    Update file_with_secrets.py

--- detect-secrets-sarif/test_data_local/files/tmp/file_with_secrets.py
+++ detect-secrets-sarif/test_data_local/files/tmp/file_with_secrets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Will change this later.
 SUPER_SECRET_VALUES = '60b725f10c9c85c70d97880dfe8191b3', '3b5d5c3712955042212316173ccf37be'
-
+ANOTHER = '50b725f10c9c85c70d97880dfe8191b2'
 
 def main():
     print('Hello world!')
